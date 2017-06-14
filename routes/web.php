9bf6c3c2e229838f4b155c0c92d03aaab06e8f27
Dendi Handian <you@example.com>
2017-06-14 11:01:32 +0700

    adding admin auth

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('admin_home', 'AdminHomeController@index')->name('admin_home');
+Route::get('admin_login','AdminAuth\LoginController@showLoginForm')->name('admin_login');
+Route::post('admin_login','AdminAuth\LoginController@login');//->name('admin_login');
+Route::post('admin_logout','AdminAuth\LoginController@logout');//->name('admin_logout');
+
+Route::post('admin_password/email','AdminAuth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('admin_password/request','AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('admin_password.request');
+Route::post('admin_password/reset','AdminAuth\ForgotPasswordController@reset');
+Route::post('admin_password/reset/{token}','AdminAuth\ForgotPasswordController@showResetForm');
+
+Route::get('admin_register','AdminAuth\RegisterController@showRegistrationForm');
+Route::post('admin_register','AdminAuth\RegisterController@register');
+
+Route::get('home', 'HomeController@index')->name('home');
+
